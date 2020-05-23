305eab6ad62d6f28fd60e88151f76d46bb418812
Maciej Budzyński <maciejb@virtualdesign.pl>
2020-05-23 15:06:03 +0200

    tablet resolutions

--- src/components/Clips.js
+++ src/components/Clips.js
@@ -5,7 +5,7 @@ const videoSrc = "https://www.youtube.com/embed/e4a4RE540wg";
 
 const Clips = () => {
   return (
-    <div className="pages-container clips-container" id="clips">
+    <div className="clips-container" id="clips">
       <h1 className="clips-title">
         Tomalski - Szukam sensu prod. Pawko, skrecze DjZel
       </h1>
