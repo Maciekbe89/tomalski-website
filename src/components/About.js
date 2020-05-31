614f921be4bc9cb6f1be59a5fa81afc9b3fc0553
Maciej Budzyński <maciejb@virtualdesign.pl>
2020-05-31 22:34:53 +0200

    add favicon and new photo and title about

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import TmlPortait from "../assets/tml.png";
+import TmlPortait from "../assets/tmlski.jpg";
 
 const About = () => {
   const [move, setMove] = useState(0);
@@ -15,22 +15,21 @@ const About = () => {
     <div className="container-about" id="about">
       <div
         className="record"
-        style={{transform: `translateY(40%) rotate(${move}deg)`}}
+        style={{transform: `translateY(25%) rotate(${move}deg)`}}
       />
-
-      <h1 className="about-title">o mnie</h1>
+      <h1 className="about-title">Tomasz "Tomalski"</h1>
       <img className="image-about" src={TmlPortait} alt="portrait" />
       <p className="about-text">
-        Tomasz „Tomalski”, raper z warszawskich Jelonek. Swoją przygodę z rapem
-        rozpoczął już na przełomie tysiąclecia, gdzie wraz z Juniorem utworzył
-        zespół „Jelonki Skład”. Przy okazji promocji drugiej płyty w 2004 roku,
-        grupa wystąpiła w legendarnej audycji „Rap Time” prowadzonej przez Druha
-        Sławka w rozgłośni Radiostacja. Następnie w 2006 roku wyszedł pierwszy
-        solowy materiał pt: ”Ukryty Sens”. Po niespełna dwóch latach, w 2008
-        roku wyszedł kolejny grupowy projekt, MłodyBe&Tomal „Na kartkach pasja”.
-        Z materiałem udało się zagrać kilkadziesiąt koncertów, wystąpić w
-        licznych konkursach i rapowych audycjach radiowych. Na następną płytę
-        przyszło czekać 12 lat - „0.01 Klimatyczne EP 2020”, to najnowszy solowy
+        raper z warszawskich Jelonek. Swoją przygodę z rapem rozpoczął już na
+        przełomie tysiąclecia, gdzie wraz z Juniorem utworzył zespół „Jelonki
+        Skład”. Przy okazji promocji drugiej płyty w 2004 roku, grupa wystąpiła
+        w legendarnej audycji „Rap Time” prowadzonej przez Druha Sławka w
+        rozgłośni Radiostacja. Następnie w 2006 roku wyszedł pierwszy solowy
+        materiał pt: ”Ukryty Sens”. Po niespełna dwóch latach, w 2008 roku
+        wyszedł kolejny grupowy projekt, MłodyBe&Tomal „Na kartkach pasja”. Z
+        materiałem udało się zagrać kilkadziesiąt koncertów, wystąpić w licznych
+        konkursach i rapowych audycjach radiowych. Na następną płytę przyszło
+        czekać 12 lat - „0.01 Klimatyczne EP 2020”, to najnowszy solowy
         materiał. Prywatnie Tomalski jest kolekcjonerem płyt cd i vinyli z
         polskim rapem.
       </p>
